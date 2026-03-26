7dec15afc5df2aa2b96d71d2dba85bc6f2f209f4
Tanveer Singh Dhawan <57829318+tanveertnk11@users.noreply.github.com>
2026-03-26 17:01:51 +0700

    Refactor app.js for consistency and readability

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const OS = require('os');
@@ -10,17 +11,16 @@ const cors = require('cors')
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
-
 mongoose.connect(process.env.MONGO_URI, {
     user: process.env.MONGO_USERNAME,
     pass: process.env.MONGO_PASSWORD,
     useNewUrlParser: true,
     useUnifiedTopology: true
-}, function(err) {
+}, function (err) {
     if (err) {
         console.log("error!! " + err)
     } else {
-      //  console.log("MongoDB Connection Successful")
+        //  console.log("MongoDB Connection Successful")
     }
 })
 
@@ -38,11 +38,11 @@ var planetModel = mongoose.model('planets', dataSchema);
 
 
 
-app.post('/planet',   function(req, res) {
-   // console.log("Received Planet ID " + req.body.id)
+app.post('/planet', function (req, res) {
+    // console.log("Received Planet ID " + req.body.id)
     planetModel.findOne({
         id: req.body.id
-    }, function(err, planetData) {
+    }, function (err, planetData) {
         if (err) {
             alert("Ooops, We only have 9 planets and a sun. Select a number from 0 - 9")
             res.send("Error in Planet Data")
@@ -52,12 +52,12 @@ app.post('/planet',   function(req, res) {
     })
 })
 
-app.get('/',   async (req, res) => {
+app.get('/', async (req, res) => {
     res.sendFile(path.join(__dirname, '/', 'index.html'));
 });
 
 
-app.get('/os',   function(req, res) {
+app.get('/os', function (req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.send({
         "os": OS.hostname(),
@@ -65,14 +65,14 @@ app.get('/os',   function(req, res) {
     });
 })
 
-app.get('/live',   function(req, res) {
+app.get('/live', function (req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.send({
         "status": "live"
     });
 })
 
-app.get('/ready',   function(req, res) {
+app.get('/ready', function (req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.send({
         "status": "ready"
@@ -80,7 +80,7 @@ app.get('/ready',   function(req, res) {
 })
 
 app.listen(3000, () => {
-    console.log("Server successfully running on port - " +3000);
+    console.log("Server successfully running on port - " + 3000);
 })
 
 
